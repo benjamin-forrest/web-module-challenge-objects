966d1a38c8f4338b63b3636a6b556c820c860dc1
Benjamin F <84413548+bforrest-pct@users.noreply.github.com>
2021-12-09 21:26:18 -0800

    Task 5

--- index.js
+++ index.js
@@ -101,9 +101,11 @@ Write a function that creates an object with name, rating, feedback, add the new
   4. should return the resulting array
 */
 
-function addReview(/*Your Code Here */){
-  /*Your Code Here */
+function addReview(array, name, rating, feedback){
+  array.push({name: name, rating: rating, feedback: feedback});
+  return array;
 }
+console.log(addReview(reviews, 'Daniela', 5,'Beautiful atmosphere and wonderful vegan options!'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
